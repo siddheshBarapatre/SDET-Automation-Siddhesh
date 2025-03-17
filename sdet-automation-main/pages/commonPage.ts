85065bd738929d68e6b78bace1d1ed0a6c713242
Siddheshwar Barapatre <Siddheshwar.Barapatre@einfochips.com>
2025-03-17 18:55:34 +0530

    code clean

--- sdet-automation-main/pages/commonPage.ts
+++ sdet-automation-main/pages/commonPage.ts
@@ -20,6 +20,14 @@ export class CommonMethodsPage {
         this.highSchoolInfoInput = (name: string) => page.locator(`input[name="${name}"]`);
         this.valueSelector = (value: any) => page.locator(`[value=${value}]`);
         this.textName = (name: any) => page.locator(`//div[.='${name}']`);
+        this.validationPage = (valPage: any) => page.locator(`//span[.='${valPage}']`)
+        this.regionLocator = (role: string) => page.locator(`//div[@role='${role}']`);
+        this.submitButton = page.locator('div[class*=mantine-Stack-root] > button[type=button]');
+        this.editExtracurricularButton = page.locator('//span[contains(@class, "mantine-Text-root") and text()="Extracurricular Activities"]/ancestor::div[contains(@class, "mantine-Group-root")]//a[contains(@class, "mantine-Button-root") and contains(.,"Edit")]');
+        this.editHighSchoolInfo = page.locator('//span[contains(@class, "mantine-Text-root") and text()="High School Information"]/ancestor::div[contains(@class, "mantine-Group-root")]//a[contains(@class, "mantine-Button-root") and contains(.,"Edit")]');
+        this.submitBtn = page.locator('button[type="submit"]');
+        this.editEssayButton = page.locator('//span[contains(@class, "mantine-Text-root") and text()="Essay"]/ancestor::div[contains(@class, "mantine-Group-root")]//a[contains(@class, "mantine-Button-root") and contains(.,"Edit")]');
+        this.fileUploaded = page.locator('div[class*=m_6d731127] > div[class*=mantine-Group-root]');
     }
 
     async navigateToTheApplicationPage(applicationPageUrl: string) {
@@ -63,6 +71,7 @@ export class CommonMethodsPage {
     }
 
     async validateAtLeast2ExtracurricularActivitiesRequired(activityName: string, years: string, leadership: string, description: string) {
+        await expect(this.textLocator("Save")).toBeEnabled({ timeout: 50000 });
         await this.textOption("Add Entry").click();
         await this.inputPlaceholder("Short Input").fill(activityName);
         await this.inputPlaceholder("123").fill(years);
@@ -86,8 +95,7 @@ export class CommonMethodsPage {
     }
 
     async saveAndNext(name: string) {
-        await this.textLocator("Save").click();
-        await expect(this.notificationText('Application saved').first()).toBeVisible({ timeout: 50000 });
+        await this.page.waitForTimeout(1000);
         await expect(this.typeSelector('submit')).toBeVisible({ timeout: 50000 });
         await this.typeSelector('submit').click();
         await expect(this.textName(name)).not.toBeVisible({ timeout: 50000 });
@@ -103,9 +111,7 @@ export class CommonMethodsPage {
         await this.highSchoolInfoInput('contact.gpa').fill(gpa);
         await this.inputPlaceholder('Enter a date').fill(year);
         await this.page.setInputFiles('input[type="file"]', filePath);
-        await this.page.waitForTimeout(this.milliseconds);
-        await this.typeSelector('submit').click();
-        await expect(this.highSchoolInfoInput('contact.highSchoolName')).not.toBeVisible({ timeout: 100000 });
+        await expect(this.fileUploaded).toBeVisible({ timeout: 30000 });
     }
 
     async validateEachOptionShowsEssayBox() {
@@ -120,11 +126,84 @@ export class CommonMethodsPage {
     }
 
     async provideEssayAnswer(essayType: string, essayText: string) {
+        await this.page.waitForTimeout(1000);
         await this.valueSelector(essayType).click();
         await expect(this.valueSelector(essayType)).toBeChecked({ timeout: 5000 });
         await expect(this.selectorPlaceholder("Long Input")).toBeVisible({ timeout: 5000 });
         await this.selectorPlaceholder("Long Input").fill(essayText);
         await this.valueSelector(essayType).click();
         await expect(this.valueSelector(essayType)).not.toBeChecked({ timeout: 5000 });
+    }
+
+    async checkedEssayBox() {
+        const essayBoxes = ['Animals', 'School'];
+        for (const essayBox of essayBoxes) {
+            await this.valueSelector(essayBox).click();
+            await expect(this.valueSelector(essayBox)).toBeChecked({ timeout: 5000 });
+            await expect(this.selectorPlaceholder("Long Input")).toBeVisible({ timeout: 5000 });
+        }
+    }
+
+    async validateLetsGetToKnowYou(firstName: string, lastName: string, email: string, streetAddress: string, city: string, state: string, zipCode: string, country: string) {
+        await expect(this.validationPage("Lets get to know you!")).toBeVisible({ timeout: 50000 });
+        await this.validationPage("Lets get to know you!").click();
+        const allText = await this.regionLocator('region').first().innerText();
+        console.log(allText);
+        expect(allText).toContain(firstName);
+        expect(allText).toContain(lastName);
+        expect(allText).toContain(email);
+        expect(allText).toContain(streetAddress);
+        expect(allText).toContain(city);
+        expect(allText).toContain(state);
+        expect(allText).toContain(zipCode);
+        expect(allText).toContain(country);
+    }
+
+    async validateExtracurricularActivities(activityName: string) {
+        await expect(this.validationPage("Extracurricular Activities")).toBeVisible({ timeout: 50000 });
+        await this.validationPage("Extracurricular Activities").click();
+        const allText = await this.regionLocator('region').nth(1).innerText();
+        console.log(allText);
+        expect(allText).toContain(activityName);
+    }
+
+    async validateHighSchoolInformation(name: string, address: string, city: string, state: string, zipCode: string, gpa: string, year: string) {
+        await expect(this.validationPage("High School Information")).toBeVisible({ timeout: 50000 });
+        await this.validationPage("High School Information").click();
+        const allText = await this.regionLocator('region').nth(6).innerText();
+        console.log(allText);
+        expect(allText).toContain(name);
+        expect(allText).toContain(address);
+        expect(allText).toContain(city);
+        expect(allText).toContain(state);
+        expect(allText).toContain(zipCode);
+        expect(allText).toContain(gpa);
+        expect(allText).toContain(year);
+    }
+
+    async validateEssay(animal: string, essayAboutAnimals: string, school: string, essayAboutSchool: string) {
+        await expect(this.validationPage("Essay")).toBeVisible({ timeout: 50000 });
+        await this.validationPage("Essay").click();
+        const allText = await this.regionLocator('region').last().innerText();
+        console.log(allText);
+        expect(allText).toContain(animal);
+        expect(allText).toContain(essayAboutAnimals);
+        expect(allText).toContain(school);
+        expect(allText).toContain(essayAboutSchool);
+    }
+
+    async submitApplication() {
+        await this.submitButton.first().click();
+        await expect(this.submitButton.first()).not.toBeVisible({ timeout: 1500000 });
+    }
+
+    async editButtonNotVisible() {
+        await expect(this.textOption("Complete")).toBeVisible({ timeout: 50000 });
+        const currentUrl = this.page.url();
+        console.log("Current URL: ", currentUrl);
+        await expect(this.editExtracurricularButton).not.toBeVisible();
+        await expect(this.editHighSchoolInfo).not.toBeVisible();
+        await expect(this.editEssayButton).not.toBeVisible();
+        await expect(this.submitBtn).not.toBeVisible();
     }
 }
--- sdet-automation-main/tests/allTests.spec.ts
+++ sdet-automation-main/tests/allTests.spec.ts
@@ -1,17 +1,18 @@
 import { test, expect } from '@playwright/test';
-import { CommonMethodsPage } from '../pages/commonMethodsPage';
+import { CommonMethodsPage } from '../pages/commonPage';
 import comData from '../resources/commonData.json';
 import tempEmail from '../resources/tempEmail.json';
 import * as fs from 'fs/promises';
 import * as path from 'path';
 
+//run command:- npx playwright test ./tests/
 test('Register a new User and complete the application', async ({ page }) => {
-    test.setTimeout(200000);
+    test.setTimeout(1500000);
     const commonPage = new CommonMethodsPage(page);
 
     const filePath = path.join(__dirname, '../resources/attachData/MySchoolTranscript.pdf');
 
-    const testEmail = `abc+${Math.floor(Math.random() * 1000000)}@example.com`;
+    const testEmail = `abc${Math.floor(Math.random() * 1000000)}@example.com`;
     await fs.writeFile(path.resolve(__dirname, '../resources/tempEmail.json'), JSON.stringify({ testEmail }), 'utf-8');
 
     // Navigate to the application page 
@@ -48,11 +49,9 @@ test('Register a new User and complete the application', async ({ page }) => {
     // Save and proceed to the next page
     await commonPage.saveAndNext('Add Entry');
 
-
     // Page 3: Fill out the form and upload the school transcript
     await commonPage.fillOutFormAndUploadSchoolTranscript(comData.school.name, comData.school.address, comData.school.city, comData.school.state, comData.school.zipcode, comData.school.gpa, comData.school.year, filePath)
-
-
+    await commonPage.saveAndNext('High School Information');
     // Page 4: Validate essay options and provide answers
     // Validate that each option under "Please select the essay types you want to write about” shows an essay box
     await commonPage.validateEachOptionShowsEssayBox();
@@ -60,22 +59,24 @@ test('Register a new User and complete the application', async ({ page }) => {
     // Provide Answers to the Two selected Essay Questions
     await commonPage.provideEssayAnswer(comData.essays.animal, comData.essays.essayAboutAnimals);
     await commonPage.provideEssayAnswer(comData.essays.school, comData.essays.essayAboutSchool);
+    await commonPage.checkedEssayBox();
 
     // Save and proceed to the next page
     await commonPage.saveAndNext("Essay");
 
     // Review Page: Validate pages and answers
-    await expect(page.locator('text=Review your application')).toBeVisible();
-    await expect(page.locator('text=John Doe')).toBeVisible();
-    await expect(page.locator('text=Basketball')).toBeVisible();
-    await expect(page.locator('text=Essay about Animals')).toBeVisible();
+    await commonPage.validateLetsGetToKnowYou(comData.newUser.firstName, comData.newUser.lastName, tempEmail.testEmail, comData.newUser.streetAddress, comData.newUser.city, comData.newUser.state, comData.newUser.zipCode, comData.newUser.country);
+    for (let i = 1; i < comData.extracurricularActivities.length; i++) {
+        await commonPage.validateExtracurricularActivities(
+            comData.extracurricularActivities[i].activityName,
+        );
+    }
+    await commonPage.validateHighSchoolInformation(comData.school.name, comData.school.address, comData.school.city, comData.school.state, comData.school.zipcode, comData.school.gpa, comData.school.year);
+    await commonPage.validateEssay(comData.essays.animal, comData.essays.essayAboutAnimals, comData.essays.school, comData.essays.essayAboutSchool);
 
     // Submit Application
-    await page.click('text=Submit');
-    const submittedUrl = page.url();
-    await expect(page.locator('text=Application submitted successfully')).toBeVisible();
+    await commonPage.submitApplication();
 
     // Validate editing is not allowed after submission
-    await page.goto(submittedUrl);
-    await expect(page.locator('text=Editing is not allowed')).toBeVisible();
+    await commonPage.editButtonNotVisible();
 });
